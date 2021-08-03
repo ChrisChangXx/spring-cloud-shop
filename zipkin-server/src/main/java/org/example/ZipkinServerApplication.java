510fcf573c4b4eeaf3e1c5664df980e8dade0d65
chris <512364167@qq.com>
2021-08-03 23:40:35 +0800

    add zipkin

---
+++ zipkin-server/src/main/java/org/example/ZipkinServerApplication.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import zipkin.server.internal.EnableZipkinServer;
+
+/**
+ * Hello world!
+ */
+@SpringBootApplication
+@EnableZipkinServer
+public class ZipkinServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZipkinServerApplication.class);
+    }
+}
